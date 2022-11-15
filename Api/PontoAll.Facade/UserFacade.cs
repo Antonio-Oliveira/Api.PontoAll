3488c1067e6f3d3e4368a0b20725537b0153fe49
Antonio-Oliveira <antonio_jorge15@hotmail.com>
2022-11-15 20:16:01 -0300

    teste

--- Api/PontoAll.Facade/UserFacade.cs
+++ Api/PontoAll.Facade/UserFacade.cs
@@ -13,6 +13,9 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
+/// <summary>
+/// teste
+/// </summary>
 namespace PontoAll.Facade
 {
     public class UserFacade : IUserFacade
